8d2c2ac6dce19357092ef093cf01a4977936c698
Jace Benson <jace.benson@gmail.com>
2017-10-12 07:40:20 -0500

    bigupdate

--- index.js
+++ index.js
@@ -1,90 +1,162 @@
+// SNDocs
+// ======
+// SNDocs is the unofficial documentation for Servicenow 
+var config = require("./config"); /* Load the config.instances, and config.endpoints */
 var fs = require("fs");
-var download = require("download");
+var https = require("https");
+var beautify = require('js-beautify').js_beautify;
 var wd = process.env.PWD;
-var arguments = process.argv;
-var instance = 'https://dev.service-now.com';
-var out = false;
-for (var index = 0; index < arguments.length; index++) {
-  var argument = arguments[index];
-  if (argument === "-o" || argument === "--out") {
-    out = arguments[index + 1];
-  }
-}
-var arrayOfFiles = [
-  "/scripts/js_includes_sp.jsx",
-  "/sn.dragdrop.jsdbx",
-  "/scripts/glide-highcharts-v5.js",
-  "/scripts/SLARepairClient.js",
-  "/scripts/app.guided_tours/js_guided_tours_includes.jsx",
-  "/scripts/classes/doctype/js_includes_listv2_doctype.jsx",
-  "/scripts/doctype/js_includes_doctype.jsx",
-  "/scripts/doctype/js_includes_last_doctype.jsx",
-  "/scripts/doctype/z_last_include.js",
-  "/scripts/form_tags.jsx",
-  "/scripts/heisenberg/heisenberg_all.jsx",
-  "/scripts/js_includes_amb.jsx",
-  "/scripts/js_includes_customer.jsx",
-  "/scripts/js_includes_form_presence.jsx",
-  "/scripts/js_includes_list_edit_doctype.jsx",
-  "/scripts/js_includes_sorting_new.jsx",
-  "/scripts/js_includes_ui16_form.jsx",
-  "/scripts/lib/newtag-it.jsx",
-  "/scripts/magellan.CreateFavoriteModal.jsx",
-  "/scripts/transaction_scope_includes.jsx",
-  "/scripts/sn/concourse/js_includes_concourse.jsx",
-  "/scripts/ui_policy.jsx"
-];
-console.log(arrayOfFiles);
-arrayOfFiles.map(function(x) {
-  console.log(x);
-  getit(instance + x, out);
-});
-console.log("Two things left to do;");
-console.log(
-  "Make the code, pretty and make them HTML files, do that by running these"
-);
-console.log("js-beautify -q -r -f ./.sn/*.js");
-console.log("docco ./.sn/*.js");
+var versions = {
+    "helsinki": {},
+    "istanbul": {},
+    "jakarta": {}
 
-function getit(url, out, callback) {
-  try {
-    if (out && url) {
-      download(url, out).then(() => {
-        var urlArr = url.split("/");
-        var domain = url.split(".com/")[0] + ".com";
-        //console.log("Downloaded Successful");
-        if (out.charAt(out.length != "/")) {
-          out += "/";
-        }
-        var file = out + urlArr[urlArr.length - 1];
-        //console.log("Opening file: " + file);
-        //console.log("File Exists: " + fs.existsSync(file));
-        fs.readFile(file, (err, data) => {
-          if (data) {
-            data = data.toString();
-            //console.log("recieved data: " + data.length);
-            var regex = /(\/\*\! RESOURCE: )(.+)\*\//g;
-            var found_resources = data.match(regex); //array of comments
-            for (
-              var downloadIndex = 0;
-              downloadIndex < found_resources.length;
-              downloadIndex++
-            ) {
-              var resource = found_resources[downloadIndex].toString();
-              resource = resource.split("/*! RESOURCE: ")[1].split(" */")[0];
-              resource = domain + resource;
-              //console.log("Downloading: " + resource);
-              download(resource, out);
-            }
-          }
+    /**
+     * family: {
+     *      patch: 0
+     *      url: 
+     * }
+     */
+};
+//console.log(JSON.stringify(config));
+//config.instances.length = 3;
+var counter = 0;
+var instances = config.instances.map(function(url) {
+    var body = '';
+    https.get(url, (data) => {
+        data.on('data', (d) => {
+            body += d;
         });
-      });
-    } else if (!out) {
-      console.log("--out not defined");
-    } else if (!url) {
-      console.log("--url not defined");
+        data.on('end', () => {
+            addToVersions({
+                body: body,
+                url: url
+            });
+        });
+    });
+});
+
+function addToVersions(obj) {
+    var body = obj.body.toString();
+    var url = obj.url;
+    counter++;
+    var family = body.split('<br/>Build name: ')[1].split('<br/>')[0].toLowerCase();
+    var patch = body.split('__patch')[1].split('-')[0];
+    var url = url.split('/stats.do')[0];
+    var familyExists = typeof versions[family];
+    if (familyExists) {
+        var patchExists = typeof versions[family][patch];
+        if (patchExists) {
+            versions[family][patch] = url;
+        } else {
+            versions[family][patch] = url;
+        }
+    } else { // family doesn't exist, create it
+        versions[family] = {};
+        versions[family][patch] = url;
     }
-  } catch (err) {
-    console.log(err);
-  }
+    if (counter == config.instances.length) {
+        createSources();
+    }
+
+}
+
+function makeFolder(path, callback) {
+    if (!fs.existsSync(path)) {
+        fs.mkdir(path, function(err) {
+            if (err) {
+                return console.error(err);
+            }
+        });
+        makeFolder(path, callback);
+    }
+    callback();
+};
+
+function createSources() {
+    console.log(JSON.stringify(versions, '', '  '));
+    //check for directories...
+    // ./sources/family/patch
+    // ./docs/family/patch
+    var sourceFolder = './sources/';
+    makeFolder(sourceFolder, function() {
+        for (family in versions) {
+            var familyFolder = sourceFolder + family + '/';
+            makeFolder(familyFolder, function() {
+                for (patch in versions[family]) {
+                    var patchFolder = familyFolder + patch + '/';
+                    makeFolder(patchFolder, function() {
+                        downloadEndpoints({
+                            "url": versions[family][patch],
+                            "path": patchFolder
+                        });
+                    });
+                }
+            });
+        }
+    });
+}
+
+function downloadEndpoints(obj) {
+    config.endpoints.map(function(file) {
+        var justFile = file.split('/');
+        justFile = justFile[justFile.length - 1];
+        var url = obj.url + file;
+        https.get(url, (data) => {
+            var body = '';
+            data.on('data', (d) => {
+                body += d;
+            });
+            data.on('end', () => {
+                //console.log('downloading ' + obj.url + file + ' to ' + obj.path + justFile);
+                if (justFile.split('.')[1].toLowerCase() == 'js') {
+                    //if .js file download it.
+                    body = beautify(body, { indent_size: 2 }); 
+                    fs.writeFile(obj.path + justFile, body, function(err) {
+                        if (err) {
+                            console.log(err.message);
+                        }
+                    });
+                } else { //else, get RESOURCES
+                    var regex = /(\/\*\! RESOURCE: )(.+)\*\//g;
+                    var found_resources = body.match(regex); //array of comments
+                    try {
+                        if (typeof found_resources) {
+                            found_resources.map(function(jsFile) {
+                                jsFile = jsFile.split('/*! RESOURCE: ')[1].split('*/')[0];
+                                if (jsFile.indexOf('.jsx') == -1 && jsFile.indexOf('min.js') == -1 && jsFile.indexOf('_min_') == -1 && jsFile.indexOf('.js') >= 0) {
+                                    //if the file dose not include min.js in the name,
+                                    var url = obj.url + jsFile
+                                    console.log('downloading ' + jsFile + ' from ' + url);
+                                    https.get(obj.url + jsFile, (data) => {
+                                        var jsBody = '';
+                                        data.on('data', (d) => {
+                                            jsBody += d;
+                                        });
+                                        data.on('end', () => {
+                                            var justFile = jsFile.split('/');
+                                            justFile = justFile[justFile.length - 1].replace(' ','');
+                                            jsBody = beautify(jsBody, { indent_size: 2 });
+                                            fs.writeFile(obj.path + justFile, jsBody, function(err) {
+                                                if (err) {
+                                                    console.log(err.message);
+                                                }
+                                                console.log('downloaded ' + obj.path + justFile + ' from ' + url);                                                
+                                            });
+                                        })
+                                    }).on('error', (e) => {
+                                        console.error(e);
+                                    });
+                                }
+                            });
+                        }
+                    } catch (error) {
+                        console.log(error.message);
+                    }
+                }
+            });
+        }).on('error', (e) => {
+            console.error(e);
+        });
+    });
 }
