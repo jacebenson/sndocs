092e7bc4432d84c3ba7d6fffa760af2b0d320e8d
Jace Benson <jace.benson@gmail.com>
2018-01-25 14:58:47 -0600

    collapse section, instead of hide it

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,6 @@
 var toggleFeatures = function(){
     jQuery('.feature').each(function(index){
-        //jQuery(this).toggleClass("d-none");
-        jQuery(this).toggleClass("invisible");
+        jQuery(this).toggleClass("d-none");
     });
     jQuery('#feature-control').toggleClass('fa-angle-up');
     jQuery('#feature-control').toggleClass('fa-angle-down');
