0fad86f75774e569e2bbc607c0bea5f6d90d1253
Jace Benson <jacebenson@fico.com>
2019-06-24 13:14:02 -0500

    forgot to build before commiting

--- test.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var port = process.env.PORT || 8000;
-var localPath2 = path.join(__dirname, '/public');
-console.log('Running locally here => http://localhost:' + port);
-app.use(express.static(localPath2));
-app.listen(port);
